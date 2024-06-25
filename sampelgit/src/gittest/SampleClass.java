9a611d89904e598e241d02b9c9e2c2035c519417
Admin <Admin@192.168.1.45>
2024-06-25 09:59:06 +0530

    For test

---
+++ sampelgit/src/gittest/SampleClass.java
@@ -0,0 +1,13 @@
+package gittest;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		
+
+	}
+
+}
